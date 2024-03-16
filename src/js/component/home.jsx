080f1c880d6ed2a67e5efb30b5906c6efc5b8fac
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-03-16 16:21:54 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
-	// add into array -> concat
-	// clear from array -> filter
-	// update -> map
 
 	return (
 		<div className="container">
 			<h1>
 				<em>
-					<bold>My Todo List:</bold>
+					<strong>My Todo List:</strong>
 				</em>
 			</h1>
 			<ul>
@@ -25,26 +19,26 @@ const Home = () => {
 						onChange={(e) => setInputValue(e.target.value)}
 						value={inputValue}
 						onKeyDown={(e) => {
-							console.log("Enter was press");
 							if (e.key === "Enter") {
-								setTodos(todos.concat(inputValue));
+								setTodos(todos.concat({task: inputValue, date: new Date()}));
+								setInputValue(""); // Clear the input value
 							}
 						}}
 						placeholder="ToDo? "
 					/>
 				</li>
-				{todos.map((t) => (
+				{todos.map((t, index) => (
 					<li style={{ display: "flex", justifyContent: "space-between" }}>
-						{t}
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" onClick={() => setTodos (todos.filter ((t, currentIndex) => index != currentIndex))}/>
-</svg>
+						{t.task} - {t.date.toLocaleString()}
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16" onClick={() => setTodos(todos.filter((_, currentIndex) => index !== currentIndex))}>
+							<path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+						</svg>
 					</li>
 				))}
 				<div>
-					<bold>
-						<em>23 tasks</em>
-					</bold>
+					<strong>
+						<em>{todos.length} tasks</em>
+					</strong>
 				</div>
 			</ul>
 		</div>
